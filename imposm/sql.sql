cbcf4d811f1d31bce6a12299d67da5401ace3e67
Artem Tartakynov <artem.tad@gmail.com>
2017-10-30 21:39:39 +0700

    Update sql.sql

--- imposm/sql.sql
+++ imposm/sql.sql
@@ -1,23 +1,27 @@
 -- bounding box is created using QGIS with coordinate capture plugin
 -------------
 
+-- vertices.csv
 SELECT
     r.id,
     ST_AsText(r.geometry) as geometry
 FROM
     import.osm_roads r
 WHERE
+    -- bounding box
     ST_Intersects(r.geometry, ST_Envelope(ST_GeomFromText('LINESTRING(11158491.523 1594059.717, 11238889.508 1510311.816)', 3857)))
 ORDER BY
     r.id
 -------------
 
+-- edges.csv
 SELECT
   a.id, b.id
 FROM
   import.osm_roads a
   INNER JOIN import.osm_roads b ON ST_Intersects(a.geometry, b.geometry) AND a.id < b.id
 WHERE
+  -- bounding box
   ST_Intersects(a.geometry, ST_Envelope(ST_GeomFromText('LINESTRING(11158491.523 1594059.717, 11238889.508 1510311.816)', 3857)))
 ORDER BY
   a.id
